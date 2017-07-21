b344cbf63b4e2d281889d11ec58200a3ab9e5a73
Dan <danielmillst@gmail.com>
2017-07-21 11:31:37 -0400

    Core plugin yml

---
+++ src/main/java/org/cyberpwn/fusion/FusionPlugin.java
@@ -0,0 +1,18 @@
+package org.cyberpwn.fusion;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class FusionPlugin extends JavaPlugin
+{
+	@Override
+	public void onEnable()
+	{
+		
+	}
+	
+	@Override
+	public void onDisable()
+	{
+		
+	}
+}
